7fdef5e4081c60ec03897aebd7c7750da9d3b73f
EnesXVC <ien3ssq@gmail.com>
2025-05-19 06:38:54 +0300

    Update and rename count.js to index.js

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ let count = 0;
 export default function handler(req, res) {
   if (req.method === "POST") {
     count++;
-    res.status(200).json({ count });
+    res.status(200).json({ message: "Count incremented", count });
   } else if (req.method === "GET") {
     res.status(200).json({ count });
   } else {
-    res.status(405).json({ message: "Method Not Allowed" });
+    res.status(405).json({ message: "Method not allowed" });
   }
 }
